42eb8f3e0feedc3b516fc95d0692ae50bccf02b2
naciriii <nnacir1938@hotmail.com>
2018-11-09 15:22:44 +0000

    sort menus by order

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -16,7 +16,7 @@
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa {{$module->icon}}"></i><span class="app-menu__label">{{trans('modules.'.$module->name)}}</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           @if(count($module->menus))
           <ul class="treeview-menu">
-            @foreach($module->menus as $menu)
+            @foreach(collect($module->menus)->sortBy('order') as $menu)
             <li><a class="treeview-item" href="{{route($menu['route'])}}"><i class="icon fa {{$menu['icon']}}"></i> 
             {{trans('global.'.$menu['name'])}}</a></li>
             @endforeach
