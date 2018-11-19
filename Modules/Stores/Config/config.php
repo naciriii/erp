9e8ae97cfdf13705aabdffea7ea3eed2c5e30e24
naciriii <nnacir1938@hotmail.com>
2018-11-19 15:57:29 +0000

    fix

--- Modules/Stores/Config/config.php
+++ Modules/Stores/Config/config.php
@@ -4,7 +4,7 @@ return [
     'name' => 'Stores',
     'api_token' => 'nao3ieptlkrjW1RiSaAuS5C1sU6D0IXG',
     'api' => [
-    	'base_url' =>env('base_url','https://mguknerp.herokuapp.com/'),
+    	'base_url' =>env('base_url','http://mgukn.test/'),
     	'auth_url' => 'api/auth',
     	'categories_url' => 'api/categories',
     	'products_url' => 'api/products'
