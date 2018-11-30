ebee6d07536658048f1b0a76846edf2cc7f7b783
naciriii <nnacir1938@hotmail.com>
2018-11-30 15:30:48 +0000

    fix

--- Modules/Stores/Config/config.php
+++ Modules/Stores/Config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Stores',
-    'api_token' => env('INTEGRATION_TOKEN','325bfe991e4d273'),
+    'api_token' => env('INTEGRATION_TOKEN','nao3ieptlkrjW1RiSaAuS5C1sU6D0IXG'),
     'api' => [
     	'base_url' =>env('base_url','http://mgukn.test/'),
     	'auth_url' => 'api/auth',
